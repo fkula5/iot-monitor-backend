61fce6fcfeda25dcbac159fc9f0a13a16882e598
Filip Kula <f.kula@1ins.pl>
2026-01-08 19:55:42 +0100

    feat: add sensor group handler and setup routes for sensor groups

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -137,6 +137,7 @@ func main() {
 
 	sensorHandler := handlers.NewSensorHandler(sensorClient)
 	sensorTypeHandler := handlers.NewSensorTypeHandler(sensorClient)
+	sensorGroupHandler := handlers.NewSensorGroupHandler(sensorClient)
 	authHandler := handlers.NewAuthHandler(authClient)
 	dataHandler := handlers.NewWebSocketHandler(dataProcClient, sensorClient)
 
@@ -146,7 +147,7 @@ func main() {
 
 	routes.SetupSensorRoutes(apiRouter, sensorHandler)
 	routes.SetupSensorTypeRoutes(apiRouter, sensorTypeHandler)
-
+	routes.SetupSensorGroupRoutes(apiRouter, sensorGroupHandler)
 	routes.SetupDataRoutes(apiRouter, dataHandler)
 
 	r.Mount("/api", apiRouter)
